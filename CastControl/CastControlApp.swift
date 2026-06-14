40073acc7cbe2b652a0522b4ad7958737e907805
Yap Han Yang <yaphanyang09@gmail.com>
2026-06-14 16:33:10 +0800

    Initial Commit

---
+++ CastControl/CastControlApp.swift
@@ -0,0 +1,17 @@
+//
+//  CastControlApp.swift
+//  CastControl
+//
+//  Created by Yap Han Yang on 14/6/26.
+//
+
+import SwiftUI
+
+@main
+struct CastControlApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
